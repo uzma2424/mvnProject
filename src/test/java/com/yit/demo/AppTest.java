6da648dafb58244fd1845d9c16c06adefe3414b7
uzma2424 <uzmaubaid7@gmail.com>
2026-04-02 12:00:47 +0530

    first commit

---
+++ src/test/java/com/yit/demo/AppTest.java
@@ -0,0 +1,20 @@
+package com.yit.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+public void testAdd() {
+        App app = new App();
+        int result = app.add(2, 3);
+        System.out.println("Running test: 2 + 3 = " + result);
+        assertEquals(5, result); 
+
+    }
+}
